3ac3048896a309629291f3496029c372c86ba3e6
Andre Srinivasan <andre@redislabs.com>
2018-08-20 11:02:04 -0700

    Disable persistence if no options passed to script

--- modules/RedisEdge/module-entrypoint.sh
+++ modules/RedisEdge/module-entrypoint.sh
@@ -2,16 +2,22 @@
 
 ## From https://github.com/docker-library/redis/blob/master/5.0-rc
 
-set -e
+echo '$@='"$@"
+
+set -ex
 
 ## Added: Enable the hooks for the Edge runtime
-#gosu redis bash -c 'python -u /module/edge-hooks.py >/var/log/edge-hook.log 2>&1 &'
+## XXX Should this write to a log file?
 python -u /module/edge-hooks.py &
 
 # first arg is `-f` or `--some-option`
 # or first arg is `something.conf`
 if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
 	set -- redis-server "$@"
+else
+	## Disable persistence
+	## XXX Make this configurable
+	set -- redis-server --save ''	
 fi
 
 # allow the container to be started with `--user`
