0c1357858a25fee58b5d685a26de7e49aa5183cd
Andre Srinivasan <andre@redislabs.com>
2018-09-13 13:02:36 -0700

    Checkpoint

--- modules/RedisEdge/module-entrypoint.sh
+++ modules/RedisEdge/module-entrypoint.sh
@@ -1,30 +1,26 @@
-#!/bin/sh
-
-## From https://github.com/docker-library/redis/blob/master/5.0-rc
-
-echo '$@='"$@"
+#!/usr/bin/env sh
 
 set -e
 
+## From https://github.com/docker-library/redis/blob/master/5.0-rc
+
+# # first arg is `-f` or `--some-option`
+# # or first arg is `something.conf`
+# if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
+# 	set -- redis-server "$@"
+# else
+# 	## Disable persistence
+# 	## XXX Make this configurable
+# 	set -- redis-server --save ''	
+# fi
+
+# # allow the container to be started with `--user`
+# if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
+# 	chown -R redis .
+# 	exec gosu redis "$0" "$@"
+# fi
+
+redis-server --save "" --port 16739 --daemonize yes
+
 PYTHONUNBUFFERED=true
-## Added: Enable the hooks for the Edge runtime
-## XXX Should this write to a log file?
-/module/edge-hooks.py &
-
-# first arg is `-f` or `--some-option`
-# or first arg is `something.conf`
-if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
-	set -- redis-server "$@"
-else
-	## Disable persistence
-	## XXX Make this configurable
-	set -- redis-server --save ''	
-fi
-
-# allow the container to be started with `--user`
-if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
-	chown -R redis .
-	exec gosu redis "$0" "$@"
-fi
-
-exec "$@"
+exec /module/edge-hooks.py
