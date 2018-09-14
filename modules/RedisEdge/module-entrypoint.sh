da5d253b05d5a2d65066aa8d107e8d127435a74b
Andre Srinivasan <andre@redislabs.com>
2018-09-14 14:51:46 -0700

    RC1

--- modules/RedisEdge/module-entrypoint.sh
+++ modules/RedisEdge/module-entrypoint.sh
@@ -1,27 +1,26 @@
-#!/bin/sh
+#!/usr/bin/env sh
+
+set -e
 
 ## From https://github.com/docker-library/redis/blob/master/5.0-rc
 
-set -ex
+# # first arg is `-f` or `--some-option`
+# # or first arg is `something.conf`
+# if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
+# 	set -- redis-server "$@"
+# else
+# 	## Disable persistence
+# 	## XXX Make this configurable
+# 	set -- redis-server --save ''	
+# fi
 
-## Added: Enable the hooks for the Edge runtime
-## XXX Should this write to a log file?
-/module/edge-hooks.js &
+# # allow the container to be started with `--user`
+# if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
+# 	chown -R redis .
+# 	exec gosu redis "$0" "$@"
+# fi
 
-# first arg is `-f` or `--some-option`
-# or first arg is `something.conf`
-if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
-	set -- redis-server "$@"
-else
-	## Disable persistence
-	## XXX Make this configurable
-	set -- redis-server --save ''	
-fi
+redis-server --save "" --port 16739 --daemonize yes
 
-# allow the container to be started with `--user`
-if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
-	chown -R redis .
-	exec gosu redis "$0" "$@"
-fi
-
-exec "$@"
+PYTHONUNBUFFERED=true
+exec /module/edge-hooks.py
