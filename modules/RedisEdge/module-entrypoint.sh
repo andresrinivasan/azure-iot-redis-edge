db25c8dfec226d86cc55719c1f1df2e6cd8633c2
Andre Srinivasan <andre.srinivasan@gmail.com>
2018-12-03 14:14:50 -0800

    Updated to Redis 5.0.2
    Signed-off-by: Andre Srinivasan <andre.srinivasan@gmail.com>

--- modules/RedisEdge/module-entrypoint.sh
+++ modules/RedisEdge/module-entrypoint.sh
@@ -1,25 +1,5 @@
 #!/usr/bin/env sh
 
-set -e
-
-## From https://github.com/docker-library/redis/blob/master/5.0-rc
-
-# # first arg is `-f` or `--some-option`
-# # or first arg is `something.conf`
-# if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
-# 	set -- redis-server "$@"
-# else
-# 	## Disable persistence
-# 	## XXX Make this configurable
-# 	set -- redis-server --save ''	
-# fi
-
-# # allow the container to be started with `--user`
-# if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
-# 	chown -R redis .
-# 	exec gosu redis "$0" "$@"
-# fi
-
 redis-server --save "" --port 6379 --daemonize yes
 
 exec /module/edge-hooks.js
